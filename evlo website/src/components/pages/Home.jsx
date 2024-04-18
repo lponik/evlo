40aabb60af4d7015393ceeda3d736bbb8148b4be
Logan <lponik@udel.edu>
2024-04-18 19:09:21 -0400

    updated homepage

--- evlo website/src/components/pages/Home.jsx
+++ evlo website/src/components/pages/Home.jsx
@@ -3,9 +3,19 @@ const Home = () => {
     <div>
         <div>
             
+              <div className="stripes-container">
+                <img className = "stripesimg" src="/src/assets/stripes.jpeg" alt="Stripes" />
+                
+                <img className="logo" src="/src/assets/evl.png" alt="Logo" />
+                <div className = "overlay-text-container">
+                  <div className="overlay-text">Your vision, our expertise.</div>               
+                  <div className="overlay-text2">Premier Service, Premier Quality.</div>
+                  </div>
+              </div>
+              
         </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
